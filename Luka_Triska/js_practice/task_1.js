37215ea3c5fa15d4cb400178acd4168cdac93469
Luka Triska <triska@ucu.edu.ua>
2018-11-14 09:23:12 +0200

    add js

--- Luka_Triska/js_practice/task_1.js
+++ Luka_Triska/js_practice/task_1.js
@@ -1,9 +1,6 @@
-const a = 4;
-// document.write(" wawdwadd");
-
 let number = 1000;
-
 let sum = 0;
+
 for (let i = 0; i <= number; i++) {
 
     if (i % 3 === 0 || i % 5 === 0) {
@@ -12,4 +9,3 @@ for (let i = 0; i <= number; i++) {
 }
 
 console.log(sum);
-// document.write(sum);
