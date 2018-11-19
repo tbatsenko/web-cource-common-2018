f0861b68c73897ab4eb777735fa14ecd6c3a771b
mykhailoivankiv <neformal.lviv@gmail.com>
2018-11-19 17:27:30 +0200

    Add webpack to mineSweeper.

---
+++ Yuriy_Antentyk/mineSweeper/src/BEM.js
@@ -0,0 +1,56 @@
+// @flow
+import {
+  compose,
+  join,
+  keys,
+  split,
+  map,
+  concat,
+  prop,
+  identity,
+  __,
+  filter,
+  ifElse,
+  defaultTo,
+  not,
+  always,
+  equals,
+  type, trim,
+} from "ramda"
+
+const ELEMENT_SEPARATOR = "__",
+  MODIFIER_SEPARATOR = "_"
+
+export const selector = compose(join(""), map(c => "." + c), split(" "))
+
+const getBase = ({ b, e }) => (e ? b + ELEMENT_SEPARATOR + e : b)
+
+const getBEMPAth = BEM =>
+  compose(
+    //prettier-ignore
+    trim,
+    concat(getBase(BEM) + " "),
+    join(" "),
+    map(concat(getBase(BEM) + MODIFIER_SEPARATOR)),
+    prop("m"),
+  )(BEM)
+
+export default b => (elementName, modifiers) => {
+  modifiers = ifElse(
+    //prettier-ignore
+    compose(not, equals("String"), type),
+    defaultTo({}),
+    always(modifiers),
+  )(elementName)
+
+  return getBEMPAth({
+    b,
+    e: typeof elementName === "string" ? elementName : null,
+    m: ifElse(
+      //prettier-ignore
+      Array.isArray,
+      identity,
+      compose(filter(prop(__, modifiers)), keys),
+    )(modifiers),
+  })
+}
