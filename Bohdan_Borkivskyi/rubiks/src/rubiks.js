3ca2ecb16b2771fb2c32da4208cebe592dee27ef
Bohdan <bohdan.borkivskyi@gmail.com>
2018-11-21 00:40:15 +0200

    correct typos

--- Bohdan_Borkivskyi/rubiks/src/rubiks.js
+++ Bohdan_Borkivskyi/rubiks/src/rubiks.js
@@ -356,7 +356,7 @@ function help_clicked(event) {
   if (help_window.classList.contains('help--small')) {
     help_window.classList.remove('help--small')
     help_window.classList.add('help--big')
-    help_window.innerHTML += '<p>Keys on keyboard corresponds to face rotations, slice turns and whole cube rotations, as showed <a href=\'https://ruwix.com/the-rubiks-cube/notation/\' title=\'rubiks cube notation\'>here</a></p><p>To do counterclockwise move press single Shift key before move</p>'
+    help_window.innerHTML += '<p>Keys on keyboard correspond to face rotations, slice turns and whole cube rotations, as shown <a href=\'https://ruwix.com/the-rubiks-cube/notation/\' title=\'rubiks cube notation\'>here</a></p><p>To do counterclockwise move press single Shift key before move</p>'
   } else {
     help_window.classList.remove('help--big')
     help_window.innerText = ''
