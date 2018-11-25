3eb673ee1cc1886f4f7f66cbc181567f9e4db5f4
Bodi44 <shtohrinets@ucu.edu.ua>
2018-11-25 02:44:34 +0200

    Pacman: added Webpack bundler, refactored code, divided by modules

--- Bohdan_Shtohrinets/JavaScript_practice/Packman/src/game.js
+++ Bohdan_Shtohrinets/JavaScript_practice/Packman/src/game.js
@@ -1,102 +1,9 @@
-const FIELD_SIZE = 20
-
-let field = [
-    Array(FIELD_SIZE).fill(0),
-    [0, 3, 0, 0, 3, 0, 0, 3, 0, 3, 3, 0, 3, 0, 0, 3, 0, 0, 3, 0],
-    [0, 3, 0, 0, 3, 0, 0, 3, 0, 3, 3, 0, 3, 3, 3, 3, 0, 0, 3, 0],
-    [0, 3, 3, 3, 3, 0, 0, 3, 0, 3, 3, 0, 3, 0, 0, 3, 3, 3, 3, 0],
-    [0, 3, 0, 0, 3, 3, 3, 3, 0, 3, 3, 0, 3, 0, 0, 3, 0, 0, 3, 0],
-    [0, 3, 0, 0, 3, 0, 0, 3, 0, 3, 3, 0, 3, 0, 0, 3, 0, 0, 3, 0],
-    [0, 3, 0, 0, 3, 0, 0, 3, 3, 3, 3, 3, 3, 0, 0, 3, 0, 0, 3, 0],
-    [0, 3, 0, 0, 3, 0, 0, 3, 3, 3, 3, 3, 3, 0, 0, 3, 0, 0, 3, 0],
-    [0, 3, 0, 0, 3, 0, 0, 3, 0, 0, 0, 0, 3, 0, 0, 3, 0, 0, 3, 0],
-    [0, 3, 0, 0, 3, 3, 3, 3, 3, 3, 3, 3, 3, 0, 0, 3, 3, 3, 3, 0],
-    [0, 3, 3, 3, 3, 0, 0, 3, 3, 3, 3, 0, 3, 3, 3, 3, 0, 0, 3, 0],
-    [0, 3, 0, 0, 3, 0, 0, 3, 3, 3, 3, 0, 3, 0, 0, 3, 0, 0, 3, 0],
-    [0, 3, 0, 0, 3, 0, 0, 3, 0, 0, 0, 0, 3, 0, 0, 3, 0, 0, 3, 0],
-    [0, 3, 0, 0, 3, 0, 0, 3, 3, 3, 3, 3, 3, 0, 0, 3, 0, 0, 3, 0],
-    [0, 3, 0, 0, 3, 0, 0, 3, 3, 3, 3, 3, 3, 0, 0, 3, 0, 0, 3, 0],
-    [0, 3, 0, 0, 3, 3, 3, 3, 0, 3, 3, 0, 3, 0, 0, 3, 0, 0, 3, 0],
-    [0, 3, 3, 3, 3, 0, 0, 3, 0, 3, 3, 0, 3, 3, 3, 3, 3, 3, 3, 0],
-    [0, 3, 0, 0, 3, 0, 0, 3, 0, 3, 3, 0, 3, 0, 0, 3, 0, 0, 3, 0],
-    [0, 3, 0, 0, 3, 0, 0, 3, 0, 3, 3, 0, 3, 0, 0, 3, 0, 0, 3, 0],
-    Array(FIELD_SIZE).fill(0)
-]
-
-const pacman = [[10, 12]]
-const ghost = [[10, 9]]
-
-let visitedFrames = {}
-let score = 0
+import getCharacter from './character'
+import {field, pacman, ghost} from './field'
+import renderField from './render'
 
 let gameOver = false
-
-
-const addToVisited = (element) => {
-    if (!(visitedFrames.hasOwnProperty(element.join(""))))
-        visitedFrames[element.join("")] = element
-}
-
-
-const pacmanRotation = (direction) => {
-    let degrees = {
-        "TOP": `<div class="game__cell pacman pacman_top"></div>`,
-        "BOTTOM": `<div class="game__cell pacman pacman_bottom"></div>`,
-        "LEFT": `<div class="game__cell pacman pacman_left"></div>`,
-        "RIGHT": `<div class="game__cell pacman pacman_right"></div>`
-    }
-
-    return degrees[direction]
-}
-
-const renderCell = (cellNumber, pacmanDirection) => {
-    if (cellNumber === 0)
-        return `<div class="game__cell game__cell_wall"></div>`
-
-    else if (cellNumber === 1)
-        return pacmanRotation(pacmanDirection)
-
-    else if (cellNumber === 2)
-        return `<div class="game__cell ghost">
-                <div class="ghost__eyes"></div>
-            </div>`
-
-    else if (cellNumber === 3)
-        return `<div class="game__cell game__cell_with-coin"></div>`
-
-    return `<div class="game__cell"></div>`
-}
-
-const renderField = (field, container, direction = []) => {
-    let counter = 0
-    container.innerHTML = `
-        <div class="game">
-        <div class="game__field">
-        ${field
-        .map(
-            (row) => {
-                let result = []
-                for (let i = 0; i < row.length; i++) {
-                    if (visitedFrames.hasOwnProperty([counter, i].join("")) && row[i] > 2)
-                        result.push(renderCell(4, direction))
-                    else
-                        result.push(renderCell(row[i], direction))
-                }
-
-                counter++
-                return `<div class="game__row">${result.join('')}</div>`
-            }
-        )
-        .join('')}
-        </div>
-         <h1 class="game__score">Score: ${score}</h1>
-        </div>
-    `
-}
-
-
-
-renderField(field, document.body)
+let direction = 'RIGHT'
 
 const KEY_DOWN = 40
 const KEY_LEFT = 37
@@ -110,8 +17,6 @@ const addObjectToField = (field, coordinates, object = 1) => {
     return newField
 }
 
-checkIfWall = (coordinate) => field[coordinate[0]][coordinate[1]] === 0
-
 const getRandomDirection = () => {
     let directions = ["TOP", "BOTTOM", "RIGHT", "LEFT"]
     let randomIndex = Math.floor(Math.random() * directions.length);
@@ -119,53 +24,7 @@ const getRandomDirection = () => {
     return directions[randomIndex]
 }
 
-const getCharacter = (character, direction, characterName) => {
-    if (direction === 'BOTTOM') {
-        const [x, y] = character[0]
-        if (checkIfWall([x, y + 1]))
-            return [x, y]
-
-        if (characterName === "pacman")
-            addToVisited([x, y])
-
-        return [x, y + 1]
-    }
-
-    if (direction === 'TOP') {
-        const [x, y] = character[0]
-        if (checkIfWall([x, y - 1]))
-            return [x, y]
-
-        if (characterName === "pacman")
-            addToVisited([x, y])
-
-        return [x, y - 1]
-    }
-
-    if (direction === 'LEFT') {
-        const [x, y] = character[0]
-        if (checkIfWall([x - 1, y]))
-            return [x, y]
-
-        if (characterName === "pacman")
-            addToVisited([x, y])
-
-        return [x - 1, y]
-    }
-
-    if (direction === 'RIGHT') {
-        const [x, y] = character[0]
-        if (checkIfWall([x + 1, y]))
-            return [x, y]
-
-        if (characterName === "pacman")
-            addToVisited([x, y])
-
-        return [x + 1, y]
-    }
-}
-
-let direction = 'RIGHT'
+renderField(field, document.body)
 
 setInterval(() => {
     const pacmanHead = getCharacter(pacman, direction, "pacman")
@@ -178,15 +37,13 @@ setInterval(() => {
     ghost.shift()
     ghost.push(ghostHead)
 
-    score = Object.keys(visitedFrames).length
-
-    if (pacmanHead.join("") === ghostHead.join("")) {
+    if (pacmanHead.join("") === ghost[0].join("") || pacman[0].join("") === ghostHead.join("")) {
         console.log("game over!!!")
         gameOver = true
     }
+
     if (!gameOver) renderField(fieldWithPacmanAndGhost, document.body, direction)
 }, 350)
-
 
 document.addEventListener('keypress', e => {
     switch (e.keyCode) {
