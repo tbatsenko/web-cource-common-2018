1acd5b4f923f48e32f2adad11899dd82950082e7
AntonBorkivskyy <anton.borkivskyi@gmail.com>
2018-11-27 19:36:42 +0200

    todo: JS improvements: remove

--- Anton_Borkivskyi/todo/main.js
+++ Anton_Borkivskyi/todo/main.js
@@ -1,27 +1,45 @@
-class Item{
-  constructor(value){
-    this.value = value;
-    this.checked = false;
+class Item {
+  constructor(id, value) {
+    this.id = id
+    this.value = value
+    this.checked = false
   }
 }
 
 
-class ToDo{
-  constructor(){
-    this.todo_array = [new Item('Buy potato'), new Item('Go to TMVK'), new Item('Verstaty TODO List')]
+class ToDo {
+  constructor() {
+    this.todo_array = [new Item(0, 'Buy potato2'), new Item(1, 'Go to TMVK'), new Item(2, 'Verstaty TODO List')]
   }
 
-  add(value){
-    var input = document.getElementById("input");
-    var new_item = input.value;
-    this.todo_array.push(new Item(new_item))
-    document.getElementById('main').innerHTML += '<div class="item" tabindex="1">' + new_item + '</div>'
-    input.value = "";
+
+  build_list() {
+    document.getElementById('main').innerHTML = ''
+    for (var i = 0; i < this.todo_array.length; i++) {
+      document.getElementById('main').innerHTML += '<div class="item"><p class="item__text">' + this.todo_array[i].value + '</p><button class="cancel_button" onclick="todo.remove('+ i +')"><img class="cancel_image" src="images/cancel.svg"></button></div>'
+    }
   }
 
-  build_list(){
+  add() {
+    var input = document.getElementById('input')
+    var new_item = input.value.trim()
+    if (new_item !== '') {
+      this.todo_array.push(new Item(this.todo_array.length, new_item))
+      this.build_list()
+    }
+    input.value = ''
+  }
+
+  remove(i){
+    this.todo_array.splice(i, 1)
+    this.build_list()
+  }
+
+  check(){
 
   }
+
+
 
 }
 
