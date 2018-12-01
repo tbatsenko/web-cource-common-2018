b3942d207985fdae45c2a5eb901c4bb158aa0ca9
AntonBorkivskyy <anton.borkivskyi@gmail.com>
2018-12-01 13:46:20 +0200

    calendar: header functionality

--- Anton_Borkivskyi/calendar/main.js
+++ Anton_Borkivskyi/calendar/main.js
@@ -1,15 +1,39 @@
 class Calendar{
   constructor() {
     this.months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
-    this.build_calendar(2018, 9);
+    var date = new Date()
+    this.year = date.getFullYear()
+    this.month = date.getMonth()
+    this.build_calendar(this.year, this.month);
+
   }
 
 
   build_calendar(year, month){
-    var header = this.months[month - 1] + " " + year;
-    document.getElementById("calendar__header").innerHTML = '<h1 class="calendar__header-text">'+header+'</h1>';
+    var header = this.months[month] + " " + year;
+    document.getElementById("calendar__header-text").innerHTML = header;
   }
+
+  next_month(){
+    this.month += 1
+    if(this.month === 12){
+      this.month = 0
+      this.year += 1
+    }
+    this.build_calendar(this.year, this.month)
+  }
+
+  prev_month(){
+    this.month -= 1
+    if(this.month === -1){
+      this.month = 11
+      this.year -= 1
+    }
+    this.build_calendar(this.year, this.month)
+  }
+
+
 }
 
 
-var clndr = new Calendar()
+var calendar = new Calendar()
