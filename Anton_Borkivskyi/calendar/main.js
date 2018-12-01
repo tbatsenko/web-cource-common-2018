c7b90da61937fc8379d1494fed6a5f91b970f35e
AntonBorkivskyy <anton.borkivskyi@gmail.com>
2018-12-01 18:39:06 +0200

    calendar: day cells

--- Anton_Borkivskyi/calendar/main.js
+++ Anton_Borkivskyi/calendar/main.js
@@ -1,17 +1,51 @@
 class Calendar{
   constructor() {
     this.months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
-    var date = new Date()
-    this.year = date.getFullYear()
-    this.month = date.getMonth()
-    this.build_calendar(this.year, this.month);
+    this.date = new Date()
+    this.year = this.date.getFullYear()
+    this.month = this.date.getMonth()
+    this.build_calendar();
 
   }
 
 
-  build_calendar(year, month){
-    var header = this.months[month] + " " + year;
-    document.getElementById("calendar__header-text").innerHTML = header;
+  build_calendar(){
+    var header = this.months[this.month] + " " + this.year;
+    document.getElementById("header__text").innerHTML = header;
+
+    var blank_cells_before = this.get_blank_lines_before()
+
+    var days_in_month = new Date(this.year, this.month + 1, 0).getDate();
+
+    var blank_cells_after = 42 - blank_cells_before - days_in_month
+
+    var calendar_body_html = ""
+
+    for(var i = 0; i < blank_cells_before; i++){
+      calendar_body_html += '<div class="calendar__cell"></div>'
+    }
+
+    for(var j = 1; j <= days_in_month; j++){
+      calendar_body_html += '<div class="calendar__cell">' + j + '</div>'
+    }
+
+    for(var k = 0; k < blank_cells_after; k++){
+      calendar_body_html += '<div class="calendar__cell"></div>'
+    }
+
+    // document.getElementById("calendar").innerHTML = blank_cells_before + " " + days_in_month + " " + blank_cells_after;
+    document.getElementById("calendar").innerHTML = calendar_body_html
+  }
+
+  get_blank_lines_before(){
+    var first_day = new Date(this.year + "-" + (this.month + 1) + "-01").getDay()
+    var blank_cells_before = first_day - 1
+
+    if (first_day <= 1){
+      blank_cells_before = first_day + 6
+    }
+
+    return blank_cells_before
   }
 
   next_month(){
@@ -20,7 +54,7 @@ class Calendar{
       this.month = 0
       this.year += 1
     }
-    this.build_calendar(this.year, this.month)
+    this.build_calendar()
   }
 
   prev_month(){
@@ -29,7 +63,7 @@ class Calendar{
       this.month = 11
       this.year -= 1
     }
-    this.build_calendar(this.year, this.month)
+    this.build_calendar()
   }
 
 
