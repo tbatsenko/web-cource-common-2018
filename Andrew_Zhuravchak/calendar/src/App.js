6930a1f06f9f3663b38276d024d86da2833ec90d
retsediv <retsediv1@gmail.com>
2018-12-14 21:33:08 +0200

    Very basic calendar component(only view)

--- Andrew_Zhuravchak/calendar/src/App.js
+++ Andrew_Zhuravchak/calendar/src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import  { Calendar } from './components/Calendar/Calendar.js'
-import './App.css';
+import './App.scss';
 
 class App extends Component {
   render() {
--- Andrew_Zhuravchak/calendar/src/components/Calendar/Calendar.js
+++ Andrew_Zhuravchak/calendar/src/components/Calendar/Calendar.js
@@ -1,13 +1,33 @@
 import React, { Component } from 'react'
 import './Calendar.scss'
+import './Day.scss'
+
 
 export class Calendar extends Component {
+
+  daysInMonth(iMonth, iYear) {
+    return 32 - new Date(iMonth, iYear).getDate()
+  }
+
+
   render() {
+    const days_of_week = ['mn', 'tu', 'wd', 'th', 'fr', 'sa', 'su']
+
+    let actualDate = new Date()
+
+    let days = [...Array(this.daysInMonth(actualDate.getMonth(), actualDate.getFullYear())).keys()];
+
     return (
       <div className="Calendar">
         <div className="Calendar__title">December 2018</div>
-        <div className="Calendar__header">MO, TU, TH, ...</div>
-        <div className="Calendar__days">table with days here</div>
+
+        <div className="Calendar__days-of-week">
+          {days_of_week.map((day) => <div className='day day--week' key={day}>{day}</div>)}
+        </div>
+
+        <div className="Calendar__days">
+          {days.map((day_number) => <div className='day day--number' key={day_number}>{day_number}</div>)}
+        </div>
       </div>
     )
   }
