70fbd9d37e2c3d0ca644eeef54c5e2a46297c82c
irynakostyshyn <i.kostyshyn@ucu.edu.ua>
2018-12-17 13:38:53 +0200

    change

---
+++ Iryna_Kostyshyn/tictactoe/src/index.js
@@ -0,0 +1,16 @@
+class Board extends React.Component {
+  renderSquare(i) {
+    return <Square value={i} />;
+  }
+}
+
+class Square extends React.Component {
+  render() {
+    return (<button className="square">{this.props.value}</button>);
+  }
+}
+
+
+
+
+
