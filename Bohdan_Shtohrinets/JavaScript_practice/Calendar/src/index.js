21e4dea48413c9913117999b0e2c5dd0d9831866
Bodi44 <shtohrinets@ucu.edu.ua>
2018-12-17 22:54:37 +0200

    refactored calendar

--- Bohdan_Shtohrinets/JavaScript_practice/Calendar/src/index.js
+++ Bohdan_Shtohrinets/JavaScript_practice/Calendar/src/index.js
@@ -1,22 +1,20 @@
 import Calendar from './calendar'
-import Render from './render'
 
 
 let calendar = new Calendar()
 calendar.calendarUpdate()
-Render.generateCalendar(calendar.month, calendar.year, calendar.monthsList)
-
+calendar.render()
 
 document.getElementById("next").addEventListener('click', () => {
-    calendar.year = (calendar.month === 11) ? calendar.year + 1 : calendar.year;
-    calendar.month = (calendar.month + 1) % 12;
-    calendar.calendarUpdate()
-    Render.generateCalendar(calendar.month, calendar.year, calendar.monthsList)
+  calendar.year = (calendar.month === 11) ? calendar.year + 1 : calendar.year
+  calendar.month = (calendar.month + 1) % 12
+  calendar.calendarUpdate()
+  calendar.render()
 })
 
 document.getElementById("previous").addEventListener('click', () => {
-    calendar.year = (calendar.month === 0) ? calendar.year - 1 : calendar.year;
-    calendar.month = (calendar.month === 0) ? 11 : calendar.month - 1;
-    calendar.calendarUpdate()
-    Render.generateCalendar(calendar.month, calendar.year, calendar.monthsList)
+  calendar.year = (calendar.month === 0) ? calendar.year - 1 : calendar.year
+  calendar.month = (calendar.month === 0) ? 11 : calendar.month - 1
+  calendar.calendarUpdate()
+  calendar.render()
 })
