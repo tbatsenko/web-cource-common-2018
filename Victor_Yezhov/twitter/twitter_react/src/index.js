3ccb373d6f8c2a52386abb79c7f985358948a09d
Victor Yezhov <yezhov.victor@gmail.com>
2018-12-25 17:09:44 +0200

    rewrite login on react

---
+++ Victor_Yezhov/twitter/twitter_react/src/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Login from "./components/login_components/Login"
+
+
+function App(){
+    return (
+        <div>
+            <h1>HELLO FROM REACT</h1>
+        </div>
+    )
+}
+
+
+
+ReactDOM.render(<Login />, document.getElementById('root'));
+
+
