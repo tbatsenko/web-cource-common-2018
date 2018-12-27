cf2a0d2eeda83e72675fb09d22b1d3c06c61aced
Bohdan <bohdan.borkivskyi@gmail.com>
2018-12-28 01:19:39 +0200

    add data parsing

--- Bohdan_Borkivskyi/visualization/src/components/App/App.js
+++ Bohdan_Borkivskyi/visualization/src/components/App/App.js
@@ -1,9 +1,34 @@
 import React, { Component } from 'react'
 import './App.scss'
 import Chart from '../Chart/Chart'
+import { csvParse } from 'd3'
 
 export default class App extends Component {
+  state = {
+    data: null,
+    currentYear: 1990,
+  }
+
+  constructor() {
+    super()
+    this.getData()
+  }
+
   render() {
     return <Chart />
+  }
+
+  getData = async () => {
+    let fetched = await fetch('/data.csv')
+    let row_data = await fetched.text()
+    let parsed = csvParse(row_data, ({ year, age, men, women }) => ({
+      year: Number(year),
+      age: Number(age),
+      men: Number(men),
+      women: Number(women),
+    }))
+    let filtered = parsed.filter(({ age }) => !isNaN(age))
+
+    this.setState({data: filtered})
   }
 }
