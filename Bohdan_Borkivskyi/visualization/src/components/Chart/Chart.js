fbd38f6b47a3d82bacce393154682891ca4cde20
Bohdan <bohdan.borkivskyi@gmail.com>
2018-12-28 13:24:44 +0200

    add slider for changing year; pass data to Chart

--- Bohdan_Borkivskyi/visualization/src/components/Chart/Chart.js
+++ Bohdan_Borkivskyi/visualization/src/components/Chart/Chart.js
@@ -3,6 +3,11 @@ import './Chart.scss'
 
 export default class Chart extends Component {
   render() {
-    return <h1>Chart here</h1>
+    return (
+      <React.Fragment>
+        <h1>Chart here</h1>
+        {this.props.population.map((item, index) => <p key={index}>{String(item)}</p>)}
+      </React.Fragment>
+    )
   }
 }
