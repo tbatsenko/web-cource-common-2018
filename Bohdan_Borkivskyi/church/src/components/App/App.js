339cee02460a23d1cb2542d0ba80ca8e44984451
Bohdan <bohdan.borkivskyi@gmail.com>
2018-12-28 17:57:48 +0200

    add christians; cristian disappears when antichrist touches him

--- Bohdan_Borkivskyi/church/src/components/App/App.js
+++ Bohdan_Borkivskyi/church/src/components/App/App.js
@@ -1,32 +1,12 @@
 import React, { Component } from 'react'
 import './App.css'
 import Antichrist from '../Antichrist/Antichrist'
+import Cristian from '../Cristian/Cristian'
 
 export default class App extends Component {
   state = {
-    antichrists: [
-      {
-        id: 1,
-        x: 20,
-        y: 30,
-        xMove: 3,
-        yMove: 4,
-      },
-      {
-        id: 2,
-        x: 70,
-        y: 180,
-        xMove: 5,
-        yMove: 2,
-      },
-    ],
-    cristians: [
-      {
-        id: 1,
-        x: 0,
-        y: 0,
-      },
-    ],
+    antichrists: null,
+    cristians: null,
   }
 
   constructor() {
@@ -36,13 +16,19 @@ export default class App extends Component {
     this.handleAntichristClick = this.handleAntichristClick.bind(this)
   }
 
-
-
   recalculatePosition() {
+    if (this.state.antichrists === null) {
+      return
+    }
     let anti = [...this.state.antichrists].map(obj => {
       return { ...obj }
     })
+    let cristi = [...this.state.cristians].map(obj => {
+      return { ...obj }
+    })
+    let punished = []
     for (let i = 0; i < anti.length; ++i) {
+      let punishment = false
       if (anti[i].x <= 0 || anti[i].x >= 485) {
         anti[i].xMove *= -1
       }
@@ -53,32 +39,108 @@ export default class App extends Component {
 
       anti[i].x += anti[i].xMove
       anti[i].y += anti[i].yMove
-      if(anti[i].x < 0){anti[i].x = 0}
-      if(anti[i].y < 0){anti[i].y = 0}
-      if(anti[i].x > 485){anti[i].x = 485}
-      if(anti[i].y > 285){anti[i].y = 285}
+      if (anti[i].x < 0) {
+        anti[i].x = 0
+      }
+      if (anti[i].y < 0) {
+        anti[i].y = 0
+      }
+      if (anti[i].x > 485) {
+        anti[i].x = 485
+      }
+      if (anti[i].y > 285) {
+        anti[i].y = 285
+      }
+      if (this.state.cristians === null) {
+        continue
+      }
+      for (let j = 0; j < this.state.cristians.length; j++) {
+        let cristi = this.state.cristians[j]
+        if (
+          anti[i].x + 15 >= cristi.x &&
+          anti[i].x - 15 <= cristi.x &&
+          anti[i].y + 15 >= cristi.y &&
+          anti[i].y - 15 <= cristi.y
+        ) {
+          punished.push(cristi.id)
+          punishment = true
+        }
+      }
+      if (punishment) {
+        anti[i].xMove *= -1
+        anti[i].yMove *= -1
+        anti[i].x += anti[i].xMove
+        anti[i].y += anti[i].yMove
+      }
     }
-    this.setState({ antichrists: anti })
+
+    cristi = cristi.filter(cristian => !punished.includes(cristian.id))
+    this.setState({ antichrists: anti, cristians: cristi })
   }
 
-  generate_random_antichrists(n) {
+  calculateCristianPosition(index) {
+    let baselineX = 450
+    let baselineY = 150
+    let groupNumber = 0
+    let inGroupNumber = 0
+    let groupCount = 1
+    let iter = 0
+    while (iter !== index) {
+      if (inGroupNumber === groupCount) {
+        groupCount += 2
+        inGroupNumber = 0
+        groupNumber += 1
+      }
+      inGroupNumber += 1
+      iter += 1
+      if (inGroupNumber === groupCount) {
+        groupCount += 2
+        inGroupNumber = 0
+        groupNumber += 1
+      }
+    }
+    let calculatedX = baselineX - groupNumber * 20
+    let calculatedY =
+      baselineY - (Math.floor(groupCount / 2) - inGroupNumber) * 20
+    return [calculatedX, calculatedY]
+  }
+
+  generateRandomAntichrists(n) {
     let anti = []
     for (let i = 0; i < n; ++i) {
       let antichrist = {
         id: i,
-        x: Math.floor(Math.random() * 200)+1,
-        y: Math.floor(Math.random() * 270)+1,
-        xMove: Math.floor(Math.random()*5+3),
-        yMove: Math.floor(Math.random()*5+3),
+        x: Math.floor(Math.random() * 200) + 1,
+        y: Math.floor(Math.random() * 270) + 1,
+        xMove: Math.floor(Math.random() * 2 + 1),
+        yMove: Math.floor(Math.random() * 2 + 1),
       }
-      antichrist.xMove = Math.random() < 0.5 ? antichrist.xMove : -antichrist.xMove
-      antichrist.yMove = Math.random() < 0.5 ? antichrist.yMove : -antichrist.yMove
+      antichrist.xMove =
+        Math.random() < 0.5 ? antichrist.xMove : -antichrist.xMove
+      antichrist.yMove =
+        Math.random() < 0.5 ? antichrist.yMove : -antichrist.yMove
       anti.push(antichrist)
     }
     this.setState({ antichrists: anti })
   }
 
+  generateCristians(n) {
+    let cristi = []
+    for (let i = 0; i < n; ++i) {
+      let cristian = {
+        id: i,
+        x: this.calculateCristianPosition(i)[0],
+        y: this.calculateCristianPosition(i)[1],
+      }
+      cristi.push(cristian)
+    }
+    this.setState({ cristians: cristi })
+  }
+
   render() {
+    if (this.state.antichrists === null || this.state.cristians === null) {
+      return <div className="App">Loading game</div>
+    }
     return (
       <div className="App">
         {this.state.antichrists.map(antichrist => (
@@ -90,21 +152,40 @@ export default class App extends Component {
             onClick={this.handleAntichristClick}
           />
         ))}
-
+        {this.state.cristians.map((cristian, index) => (
+          <Cristian
+            key={cristian.id}
+            id={cristian.id}
+            x={cristian.x}
+            y={cristian.y}
+          />
+        ))}
         <div className="Church" />
       </div>
     )
   }
 
   componentDidMount() {
-    this.generate_random_antichrists(5)
-    setInterval(this.recalculatePosition, 500)
+    console.log('mounted')
+    this.generateRandomAntichrists(5)
+    this.generateCristians(9)
+
+    setInterval(this.recalculatePosition, 70)
   }
 
   handleAntichristClick(antichrist_id) {
     let anti = this.state.antichrists.filter(
       antichrist => antichrist.id !== antichrist_id
     )
-    this.setState({ antichrists: anti })
+
+    let cristi = [...this.state.cristians].map(obj => {
+      return { ...obj }
+    })
+    cristi.push({
+      id: cristi[cristi.length-1].id+1,
+      x: this.calculateCristianPosition(cristi[cristi.length-1].id+1)[0],
+      y: this.calculateCristianPosition(cristi[cristi.length-1].id+1)[1],
+    })
+    this.setState({ antichrists: anti, cristians: cristi })
   }
 }
