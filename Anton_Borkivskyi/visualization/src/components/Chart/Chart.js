a16eac37d3f93f0c4332b0bc9ad43eca7214cd45
AntonBorkivskyy <anton.borkivskyi@gmail.com>
2019-01-05 01:41:46 +0200

    visualization: fixed data representation and axis scaling

--- Anton_Borkivskyi/visualization/src/components/Chart/Chart.js
+++ Anton_Borkivskyi/visualization/src/components/Chart/Chart.js
@@ -1,29 +1,53 @@
 import { Component } from 'react'
 import React from 'react'
-import { scaleLinear } from '@vx/vx'
-import { extent, max, curveMonotoneX } from 'd3'
-import { AreaClosed, Group, LinePath } from '@vx/vx'
-import { csvParse } from 'd3'
 
-import { AxisLeft, AxisBottom } from '@vx/vx'
+import { extent, curveMonotoneX } from 'd3'
+import {
+  scaleLinear,
+  AreaClosed,
+  AxisLeft,
+  AxisBottom,
+  AxisRight,
+} from '@vx/vx'
 
 import './Chart.css'
 
 class Chart extends Component {
+  findMaxValues() {
+    const { data } = this.props.state
+
+    let maxMen = 0
+    let maxWomen = 0
+    for (let i = 0; i < data.length; i++) {
+      // console.log(data[i])
+      if (data[i]['men'] > maxMen) {
+        maxMen = data[i]['men']
+      }
+      if (data[i]['women'] > maxWomen) {
+        maxWomen = data[i]['women']
+      }
+    }
+
+    return [maxMen, maxWomen]
+  }
 
   render() {
-    const { data, activeYear } = this.props.state
+    const { data, activeYear, chart_width } = this.props.state
 
-    const width = 800
-    const height = 400
-
+    // const width = 400
+    const width = chart_width / 2
+    const height = chart_width / 2
+    const axis_size = 50
+    let maxValues = this.findMaxValues()
+    const maxMen = maxValues[0]
+    const maxWomen = maxValues[1]
     const dataMale = []
     const dataFemale = []
     const staticDataMale = []
     const staticDataFemale = []
 
-    const dynamic_data = data.filter((row) => row['year'] === activeYear)
-    const static_data = data.filter((row) => row['year'] === 1989)
+    const dynamic_data = data.filter(row => row['year'] === activeYear)
+    const static_data = data.filter(row => row['year'] === 1989)
 
     for (let i = 0; i < 80; i++) {
       dataMale.push({ age: i, population: dynamic_data[i]['men'] })
@@ -32,19 +56,10 @@ class Chart extends Component {
       staticDataFemale.push({ age: i, population: static_data[i]['women'] })
     }
 
-
     const x = d => d.age
     const y = d => d.population
 
-
-    const margin = {
-      top: 0,
-      bottom: 0,
-      left: 0,
-      right: 0,
-    }
-
-    const xMax = width / 2 // - margin.left - margin.right
+    const xMax = width // - margin.left - margin.right
     const yMax = height // - margin.top - margin.bottom
     const yMin = height / 4
 
@@ -60,25 +75,45 @@ class Chart extends Component {
 
     const yScaleFemale = scaleLinear({
       range: [yMax, yMin],
-      domain: [0, max(staticDataFemale, y)],
+      domain: [0, maxWomen],
     })
 
     const yScaleMale = scaleLinear({
       range: [yMax, yMin],
-      domain: [0, max(staticDataMale, y)],
+      domain: [0, maxMen],
+    })
+
+    const yScaleAxis = scaleLinear({
+      range: [height - 3, 5],
+      domain: [0, 80],
+    })
+
+    const xScaleMaleAxis = scaleLinear({
+      range: [0, width],
+      domain: [maxMen * 1.33, 0],
+    })
+    const xScaleFemaleAxis = scaleLinear({
+      range: [0, width],
+      domain: [0, maxWomen * 1.33],
     })
 
     return (
       <div className={'chart'}>
-        <svg width={width / 2} height={height} className="svg male">
+        <svg height={height} width={axis_size} className={'svg'}>
+          <AxisLeft
+            scale={yScaleAxis}
+            top={0}
+            left={axis_size}
+            stroke={'white'}
+          />
+        </svg>
 
+        <svg width={width} height={height} className="svg male">
           <AreaClosed
             data={dataMale}
-
-            x={(d) => xScaleMale(d.age)}
+            x={d => xScaleMale(d.age)}
             y0={yScaleMale(0)}
-            y={(d) => yScaleMale(d.population)}
-
+            y={d => yScaleMale(d.population)}
             fill={'#e45f4d'}
             stroke={'#e45f4d'}
             curve={curveMonotoneX}
@@ -87,28 +122,21 @@ class Chart extends Component {
 
           <AreaClosed
             data={staticDataMale}
-
-            x={(d) => xScaleMale(d.age)}
+            x={d => xScaleMale(d.age)}
             y0={yScaleMale(0)}
-            y={(d) => yScaleMale(d.population)}
-
+            y={d => yScaleMale(d.population)}
             fill={'#e45f4d'}
             stroke={'#e45f4d'}
             curve={curveMonotoneX}
-
-            opacity={0.4}
+            opacity={0.5}
           />
-
         </svg>
-        <svg width={width / 2} height={height} className="svg female">
-
+        <svg width={width} height={height} className="svg female">
           <AreaClosed
             data={dataFemale}
-
-            x={(d) => xScaleFemale(d.age)}
+            x={d => xScaleFemale(d.age)}
             y0={yScaleFemale(0)}
-            y={(d) => yScaleFemale(d.population)}
-
+            y={d => yScaleFemale(d.population)}
             fill={'#489ea3'}
             stroke={'#489ea3'}
             curve={curveMonotoneX}
@@ -116,23 +144,37 @@ class Chart extends Component {
           />
           <AreaClosed
             data={staticDataFemale}
-
-            x={(d) => xScaleFemale(d.age)}
+            x={d => xScaleFemale(d.age)}
             y0={yScaleFemale(0)}
-            y={(d) => yScaleFemale(d.population)}
-
+            y={d => yScaleFemale(d.population)}
             fill={'#489ea3'}
             stroke={'#489ea3'}
             curve={curveMonotoneX}
-            opacity={0.4}
+            opacity={0.5}
           />
-
         </svg>
+
+        <svg height={height} width={axis_size} className={'svg'}>
+          <AxisRight scale={yScaleAxis} top={0} left={0} stroke={'white'} />
+        </svg>
+
+        <div className={'chart__bottom-axis'}>
+          <svg
+            height={axis_size}
+            width={width * 2 + axis_size}
+            className={'svg'}
+          >
+            <AxisBottom left={axis_size} scale={xScaleMaleAxis} numTicks={5} />
+            <AxisBottom
+              left={axis_size + width}
+              scale={xScaleFemaleAxis}
+              numTicks={5}
+            />
+          </svg>
+        </div>
       </div>
     )
   }
 }
 
 export default Chart
-
-
