b5cb63c5c9b6f4272751473480f28f697b335759
retsediv <retsediv1@gmail.com>
2019-01-06 00:08:58 +0200

    Fix styles for Firefox

--- Andrew_Zhuravchak/pretty_calendar_layout/js/Calendar.js
+++ Andrew_Zhuravchak/pretty_calendar_layout/js/Calendar.js
@@ -48,7 +48,6 @@ export default class Calendar {
     const calendarWeeksWithDays = getCalendarDays(this.currentDisplayDate)
 
     this.block.innerHTML = `
-
       <div class="calendar-layout__controls">
         <div class="toogler">
             <button class="toogler__btn">Week</button>
@@ -71,88 +70,88 @@ export default class Calendar {
 
       <div class="calendar-layout__calendar">
     
-      <table class="calendar">
-          <thead class="calendar__header">
-            <tr>
-                ${calendarDaysList.map((dayLabel) => `
-                  <td class="calendar__header-day">${dayLabel}</td>
-                `)}
-            </tr>
-          </thead>
-      
-          <colgroup>
-              <col width="220px" span="7">
-          </colgroup>
-      
-          <tbody class="calendar__body">
-          
-          ${calendarWeeksWithDays.map((week) => `
+        <table class="calendar">
+            <thead class="calendar__header">
               <tr>
-                ${week.map(day => `
-                    <td class="calendar__cell ${!day ? 'calendar__cell--empty' : ''}">
-                        <div class="day">
-                          <ul class="day__schedule-list">
-                            ${day ? this.getTasksForDate(day).map(task => `
-                            
-                                ${task.isEmpty ? `<li class="schedule-item schedule-item--empty"></li>`
+                  ${calendarDaysList.map((dayLabel) => `
+                    <td class="calendar__header-day">${dayLabel}</td>
+                  `)}
+              </tr>
+            </thead>
+        
+            <colgroup>
+                <col width="220px" span="7">
+            </colgroup>
+        
+            <tbody class="calendar__body">
+            
+            ${calendarWeeksWithDays.map((week) => `
+                <tr>
+                  ${week.map(day => `
+                      <td class="calendar__cell ${!day ? `calendar__cell--empty` : ``}">
+                          <div class="day">
+                            <ul class="day__schedule-list">
+                              ${day ? this.getTasksForDate(day).map(task => `
+                              
+                                  ${task.isEmpty ? `<li class="schedule-item schedule-item--empty"></li>`
       : `<li class="schedule-item
-                                          ${task.isNotification ? `schedule-item__notification schedule-item__notification--${task.type}">`
+                                            ${task.isNotification ? `schedule-item__notification schedule-item__notification--${task.type}">`
         : `schedule-item__task schedule-item__task--${task.type}">`}
-                                          ${task.text}
-                                        </li>`
+                                            ${task.text}
+                                          </li>`
       }                            
-                            `) : ``}
-                            </ul>
-                            
-                          <time class="day__number ${(day && day === this.currentDate) ? `day__number--current` : ``}"
-                                datetime="${day}">${(day) ? day.getDate() : ''}</time>
-                      </div>
-                    </td>  
-              `)}      
-            </tr>
-          `)}
-      
-          </tbody>
-      </table>
-      
-      <a href="#newTask" class="action-btn">+</button>
-      
-      <div class="popup" id="newTask">
-        <figure>
-        <a href="#" class="close"></a>
-        <figcaption>
-            <h3>Add new task to the calendar</h3>
-            
-            <form method="POST" action="#" id="add-new-task-form">
-            <div>
-              <label for="text">Text: </label>
-              <input id="text" required/>
-            </div>
-            
-            <div>
-              <label for="notification">Is notification: </label>
-              <input id="notification" type="checkbox"/>
-            </div>
-            
-            <div>  
-              <label for="date">Date: </label>
-              <input id="date" type="date" required/>
-            </div>
-            
-            <div>
-              <label for="type">Type: </label>
-              <select name="type" id="type">
-                <option value="easy">easy</option>
-                <option value="medium">medium</option>
-                <option value="hard">hard</option>
-               </select>
-             <div> 
-             
-             <button type="submit">Send</button> 
-            </form>
-        </figcaption>
-        </figure>
-      </div>
+                              `) : ``}
+                             </ul>
+                              
+                            <time class="day__number ${(day && day === this.currentDate) ? `day__number--current` : ``}"
+                                  datetime="${day}">${(day) ? day.getDate() : ''}</time>
+                        </div>
+                      </td>  
+                `)}      
+              </tr>
+            `)}
+        
+            </tbody>
+        </table>
+        
+        <a href="#newTask" class="action-btn">+</button>
+        
+        <div class="popup" id="newTask">
+          <figure>
+          <a href="#" class="close"></a>
+          <figcaption>
+              <h3>Add new task to the calendar</h3>
+              
+              <form method="POST" action="#" id="add-new-task-form">
+              <div>
+                <label for="text">Text: </label>
+                <input id="text" required/>
+              </div>
+              
+              <div>
+                <label for="notification">Is notification: </label>
+                <input id="notification" type="checkbox"/>
+              </div>
+              
+              <div>  
+                <label for="date">Date: </label>
+                <input id="date" type="date" required/>
+              </div>
+              
+              <div>
+                <label for="type">Type: </label>
+                <select name="type" id="type">
+                  <option value="easy">easy</option>
+                  <option value="medium">medium</option>
+                  <option value="hard">hard</option>
+                 </select>
+               <div> 
+               
+               <button type="submit">Send</button> 
+              </form>
+          </figcaption>
+          </figure>
+        </div>
       </div>
     `
 
