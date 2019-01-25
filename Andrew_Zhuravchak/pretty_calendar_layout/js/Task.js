80d0e60b46785be52ac6e5e1fb076fe0ff44b6e7
retsediv <retsediv1@gmail.com>
2019-01-25 18:34:23 +0200

    Add remove button for task

--- Andrew_Zhuravchak/pretty_calendar_layout/js/Task.js
+++ Andrew_Zhuravchak/pretty_calendar_layout/js/Task.js
@@ -1,5 +1,6 @@
 export default class Task {
   constructor(text, isNotification, isEmpty, type, date) {
+    this.id = (new Date()).getTime()
     this.text = text
     this.isNotification = isNotification
     this.isEmpty = isEmpty
