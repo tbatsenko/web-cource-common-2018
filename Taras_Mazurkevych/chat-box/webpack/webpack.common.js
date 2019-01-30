c94ac2600d5710604a3047478e758bd4b4932695
Taras Mazurkevych <mad.taras@gmail.com>
2019-01-30 12:35:32 +0200

    Wasting my time

--- Taras_Mazurkevych/chat-box/webpack/webpack.common.js
+++ Taras_Mazurkevych/chat-box/webpack/webpack.common.js
@@ -39,7 +39,7 @@ module.exports = {
         type: 'javascript/auto'
       },
       {
-        test: /\.(ico|jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2)(\?.*)?$/,
+        test: /\.(ico|jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|json)(\?.*)?$/,
         use: {
           loader: 'file-loader',
           options: {
