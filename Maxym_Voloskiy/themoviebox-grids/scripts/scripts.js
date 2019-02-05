38df5054eff07456218c71d798702d7be200c283
MaxVoloskiy <voloskiy@ucu.edu.ua>
2019-02-05 02:44:42 +0200

    updated moviebox

--- Maxym_Voloskiy/themoviebox-grids/scripts/scripts.js
+++ Maxym_Voloskiy/themoviebox-grids/scripts/scripts.js
@@ -2,6 +2,7 @@ const render = async (templateURL, dataURL, container) => {
     const source = await (await fetch(templateURL)).text();
     const template = Handlebars.compile(source);
     const data = await (await fetch(dataURL)).json();
+
     container.innerHTML = template(data);
 };
-render("main.handlebars", "json/data.json", document.body )
+render("main.handlebars", "json/data.json", document.body );
